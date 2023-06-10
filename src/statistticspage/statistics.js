112c86a8b580e33f2230948519fcefcfed82bc42
Tru-mny <mohammad2004y@gmail.com>
2023-06-10 11:42:23 +0200

    hi

---
+++ src/statistticspage/statistics.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import Navbar from '../homepage/navbar/navbar';
+import '../App.css';
+import Footer from '../homepage/footer/footer';
+import {motion} from "framer-motion";
+
+function StatisticsPage(){
+//hi
+
+return(
+  <motion.div
+  initial={{ opacity: 0 , x:20}}
+  animate={{ opacity: 1 , x:0 }}
+  transition={{ ease: "easeOut", duration: 0.5}}>
+
+
+
+
+
+<Footer></Footer>
+<Navbar></Navbar>
+</motion.div>
+);
+}
+export default StatisticsPage;
